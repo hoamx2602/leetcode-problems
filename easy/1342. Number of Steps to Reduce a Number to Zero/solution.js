d96e5b846f7a1061d56972b14f4349ad9ddf1fe9
hoamx <hoamx.work@gmail.com>
2023-12-10 09:17:05 +0700

    resolved: problem 1342

---
+++ easy/1342. Number of Steps to Reduce a Number to Zero/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let steps = 0;
+
+  while (num !== 0) {
+    num % 2 === 0 ? (num /= 2) : num--;
+    steps++;
+  }
+
+  return steps;
+};
+
+// Time Complexity = O(logn)
+// Space Complexity = O(1)
